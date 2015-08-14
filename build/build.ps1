0fae2c7b75ecb4bb14be1c42684587fa36369621
Anton Boyko <boyko.ant@live.com>
2015-08-14 16:25:43 +0000

    Updated build.ps1

--- build/build.ps1
+++ build/build.ps1
@@ -1,6 +1,6 @@
-﻿$sourceDir = (Get-ChildItem Env:AGENT_BUILDDIRECTORY).Value
-$copyDir = Join-Path -Path $sourceDir -ChildPath "copy"
-$outputFile = Join-Path -Path $sourceDir -ChildPath "build\drop.zip"
+$sourceDir = (Get-ChildItem Env:AGENT_BUILDDIRECTORY).Value
+$copyDir = Join-Path -Path (Get-ChildItem Env:AGENT_BUILDDIRECTORY).Value -ChildPath "copy"
+$outputFile = Join-Path -Path (Get-ChildItem Env:BUILD_SOURCESDIRECTORY).Value -ChildPath "build\drop.zip"
 
 Add-Type -A System.IO.Compression.FileSystem
 [IO.Compression.ZipFile]::CreateFromDirectory($copyDir, $outputFile)
