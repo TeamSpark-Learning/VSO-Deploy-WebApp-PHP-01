d3a0b60bab5f03683949eb1b5a67a0969afda22e
Boyko Anton <boyko.ant@live.com>
2015-08-14 12:25:53 +0300

    init

---
+++ index.php
@@ -0,0 +1,5 @@
+<p>&lt;?php</p>
+<p>
+<span style="font-family: 'Fira Mono', 'Source Code Pro', monospace; font-size: 14px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: 24px; orphans: auto; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px; color: rgb(0, 0, 187); background-color: rgb(255, 255, 255);">
+phpinfo</span><span style="font-family: 'Fira Mono', 'Source Code Pro', monospace; font-size: 14px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: 24px; orphans: auto; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px; color: rgb(0, 119, 0); background-color: rgb(255, 255, 255);">();</span></p>
+<p>?&gt;</p>
